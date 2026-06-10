1edaf0984aeca81ea4f81177f1fc3f50a4392237
LalbaAnthony <anthonylalaba24@gmail.com>
2026-06-10 15:06:17 +0200

    Some nuxt config

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,8 +1,23 @@
 <template>
     <div>
-        test
+        <select :value="locale" @change="switchLocale(($event.target as HTMLSelectElement).value)">
+            <option v-for="loc in locales" :key="loc.code" :value="loc.code">
+                {{ loc.name }}
+            </option>
+        </select>
+
+        <h1>{{ t('home.title') }}</h1>
+        <p>{{ t('home.description') }}</p>
     </div>
 </template>
 
 <script setup lang="ts">
+import type { Language } from '~/types/i18n';
+
+const { t, locale, locales } = useI18n()
+const switchLocalePath = useSwitchLocalePath()
+
+function switchLocale(code: string) {
+    navigateTo(switchLocalePath(code as Language))
+}
 </script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,41 @@
+import tailwindcss from '@tailwindcss/vite'
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-
+  vite: {
+    plugins: [tailwindcss()],
+  },
   modules: [
     '@nuxt/icon',
     '@nuxt/eslint',
     '@nuxtjs/i18n',
     '@nuxtjs/robots',
     '@nuxtjs/sitemap',
-    '@nuxtjs/tailwindcss'
-  ]
+  ],
+  i18n: {
+    defaultLocale: 'en',
+    locales: [
+      { code: 'en', language: 'en-US', name: 'English', file: 'en.json' },
+      { code: 'fr', language: 'fr-FR', name: 'Français', file: 'fr.json' },
+    ],
+    strategy: 'prefix_except_default',
+    detectBrowserLanguage: {
+      useCookie: true,
+      cookieKey: 'i18n_locale',
+      redirectOn: 'root',
+      alwaysRedirect: true,
+    },
+  },
+  css: [
+    '@/assets/css/main.css',
+  ],
+  icon: {
+    clientBundle: { // Bundle icon on build for better performance AND avoid nuxt icon API messing up with backend proxy (/api)
+      scan: {
+        globInclude: ['**/*.{vue,ts}'],
+      },
+    }
+  },
 })
